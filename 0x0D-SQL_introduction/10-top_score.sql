d3709733d8dab9e2f7374382d4e4d898034ff690
Majidied <majidimajidi2003@gmail.com>
2024-02-08 17:41:34 +0100

    fix

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Delete a Database hbtn_0c_0 from the system
-DROP DATABASE `hbtn_0c_0`;
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
