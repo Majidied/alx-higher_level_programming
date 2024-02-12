eb05d77dd03eca7d9ad4b60839735f1fc50317a7
Majidied <majidimajidi2003@gmail.com>
2024-02-12 16:09:14 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length < 3) {
-  console.log('0');
+  console.log(0);
 } else {
   const arr = process.argv.slice(2).map(Number);
   const second = arr.sort(function (a, b) { return b - a; })[1];
