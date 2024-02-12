5aaf5e50baa48fb801a062f758174fbae3798748
Majidied <majidimajidi2003@gmail.com>
2024-02-12 15:38:49 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {
