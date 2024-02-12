a3f55664bf168290801d964f3b31f8a32ce59961
Majidied <majidimajidi2003@gmail.com>
2024-02-12 12:55:17 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2] == undefined || isNaN(process.argv[2])) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number:', parseInt(process.argv[2]));
