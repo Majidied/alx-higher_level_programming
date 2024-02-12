5e58b31f76c333a7b25b73e8eae3688ac56dfcb3
Majidied <majidimajidi2003@gmail.com>
2024-02-12 12:36:44 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length < 3) {
+if (process.argv.length < 3) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
