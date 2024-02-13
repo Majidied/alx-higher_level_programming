24fff78169bbf358b61ea7900143d8f020456b4d
Majidied <majidimajidi2003@gmail.com>
2024-02-13 20:41:21 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (!c) { c = 'X'; }
+    if (c === undefined) { c = 'X'; }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
