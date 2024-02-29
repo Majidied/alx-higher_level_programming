bdfde69c7327b808946bc341e18ef9c8edc5bf98
Majidied <majidimajidi2003@gmail.com>
2024-02-29 20:40:12 +0100

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as argument
-if [ $# -eq 0 ]; then
-  echo "Error: URL is missing."
-  exit 1
-fi
-
-# Send request to the URL and store the response in a variable
-response=$(curl -sI "$1")
-
-# Extract the content length from the response headers
-content_length=$(echo "$response" | grep -i "content-length:" | awk '{print $2}' | tr -d '\r')
-
-# Check if content length is empty
-if [ -z "$content_length" ]; then
-  echo "Error: Failed to retrieve content length."
-  exit 1
-fi
-
-# Display the content length in bytes
-echo "Content Length: $content_length bytes"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
