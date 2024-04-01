b9569c2343bf7a90f9e8e2315bc67b5bd470d192
majidied <majidimajidi2003@gmail.com>
2024-04-01 00:41:53 +0000

    Fix email parameter format in POST request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,10 +11,11 @@ def main(argv):
     Sends a POST request to the passed URL with the email as a parameter,
     and displays the body of the response (decoded in utf-8)
     """
-    values = {'email': argv[2]}
+    values = {"email": argv[2]}
     url = argv[1]
     r = requests.post(url, data=values)
     print(r.text)
+
 
 if __name__ == "__main__":
     main(argv)
