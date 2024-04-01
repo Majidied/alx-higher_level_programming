a123501abad7c910bf50e19ede19cf4c83aeea55
majidied <majidimajidi2003@gmail.com>
2024-04-01 00:39:52 +0000

    Fix formatting in 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,12 +5,13 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
-                                                            sys.argv[1])
+                                                              sys.argv[1])
     r = requests.get(url)
     commits = r.json()
     for commit in commits[:10]:
         print(
             "{}: {}".format(
-                commit.get("sha"), commit.get("commit").get("author").get("name")
+                commit.get("sha"), commit.get("commit").get("author"
+                                                            ).get("name")
             )
         )
